aefd0ba79e00e0bac79259c9d5f6c1c91a17c56f
J-NAVEEN-KUMAR <naveen.jeruban@gmail.com>
2022-03-01 01:13:36 +0530

    minor change

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,24 +1,25 @@
 import { Chart } from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo";
-import "./home.css"
-import {userData} from "../../dummyData"
+import "./home.css";
+import { userData } from "../../dummyData";
 import WidgetSm from "../../components/widgetsm/WidgetSm";
 import WidgetLg from "../../components/widgetLg/WidgetLg";
 
-
 const Home = () => {
   return (
-  
-  <div className="Home">
-    <FeaturedInfo />
-    <div className="homeWidgets">
-      <WidgetSm />
-      <WidgetLg />
+    <div className="Home">
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        dataKey="Active Users"
+        grid
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
-    <Chart data={userData} title="User Analytics" dataKey="Active Users" grid/>
-  </div>
-  
-
   );
 };
 
